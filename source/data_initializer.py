81e1f2bf6143109aa82cdfd37fd1938ba17ac0b1
nicholasss <nicholaszingleman@gmail.com>
2024-11-15 14:40:21 -0500

    starting work on data_initialization and testing, will be called on from central code to initialize the data and check it over for inconsistencies

---
+++ source/data_initializer.py
@@ -0,0 +1,13 @@
+
+# file initializes data from json and checks it over for any logical errors
+
+import os
+from pathlib import Path
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
