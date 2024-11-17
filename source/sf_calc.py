6e73c68f49a44f23ea12364f5e8fba55ab62303f
nicholasss <nicholaszingleman@gmail.com>
2024-11-17 17:26:41 -0500

    added logic for the count argument, and adding more syntactic sugar for the recipe argument

--- source/sf_calc.py
+++ source/sf_calc.py
@@ -49,12 +49,28 @@ def main():
             machine = bd.recipes[book]["machine"]
             print("-{:15s} made in {:15s}".format(name, machine))
 
+    if args.count:
+        try:
+            global count
+            count = int(args.count)
+        except ValueError:
+            print("Please supply an integer for count. Setting as 1.")
+            count = 1
+
+        if count <= 0:
+            print("Count provided needs to be at least 1. Setting as 1.")
+            count = 1
+
     # NOTE: Recipe Argument
     if args.recipe:
         if len(args.recipe) > 1:
             print("Too many arguments provided for recipe.")
         recipe = args.recipe[0]
-        print(f"Calculating recipe for {recipe}.")
+        if count > 1:
+            s = "s"
+        else:
+            s = ""
+        print(f"Calculating recipe for {count} {recipe}{s}.")
 
 
 if __name__ == "__main__":
