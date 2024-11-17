4f83755c87d9c1132cf35d860833a591da1a7880
nicholasss <nicholaszingleman@gmail.com>
2024-11-17 12:57:35 -0500

    added core module for parsing args

---
+++ source/sf_calc.py
@@ -0,0 +1,13 @@
+# core of program
+# parses arguments
+# calls other modules for calculations and loading
+
+import sys
+
+
+def main():
+    print(sys.argv, len(sys.argv))
+
+
+if __name__ == "__main__":
+    main()
