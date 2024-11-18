9c9d69a32ccc6f8f46b19a67ebb35b1994105f2a
nicholasss <nicholaszingleman@gmail.com>
2024-11-18 15:25:37 -0500

    refactored the search class, more to do

--- source/search.py
+++ source/search.py
@@ -50,86 +50,75 @@ class Search():
         for item in self.requirements:
             self.final_items.append(item)
 
-        # calculating ratios:
-        for key in reqs:
-            item_request_count = reqs[key]
+        # calculating output ratio:
+        item_request_count = self.__count_in_dict(reqs)
         item_output_count = self.bd.recipes[item]["out"][item]
         output_to_recipe_ratio: float = (
             item_request_count / item_output_count
         )
         print("request ratio: ", output_to_recipe_ratio)
 
-        self.find_requirements(self.requirements, item_request_count)
+        self.find_requirements(self.requirements, output_to_recipe_ratio)
 
-    def find_requirements(self, reqs: dict, item_request_count: int):
+    def find_requirements(self, reqs: dict, output_to_recipe_ratio: float):
 
         items_to_visit: list = []  # list of tuples?
         items_to_visit.append(reqs)
         input_is_raw_resource: bool = False
 
         while items_to_visit != []:
-            item = items_to_visit.pop()
-            for key in item:
-                global count
-                count = item[key]
-            item = list(item.keys())[0]
-
-            # set to false every loop
+            item_page = items_to_visit.pop()
+            request_name, request_count = self.__unpack_dict(item_page)
             input_is_raw_resource = False
 
             # finding in recipes first, then resources
             try:
-                item_book = self.bd.recipes[item]
+                recipe_page = self.bd.recipes[request_name]
             except KeyError:
-                item_book = None
-            if item_book is None:
+                recipe_page = None
+            if recipe_page is None:
                 try:
-                    item_book = self.bd.resources[item]
+                    recipe_page = self.bd.resources[request_name]
                     # set here, as if its not found then
                     # it will get reset next loop
                 except KeyError:
                     print(f"Error! Unable to find '{
-                          item}' within recipes or resources book")
+                          request_name}' within recipes or resources book")
                     return
 
-            machine: str = item_book["machine"]
-            machine_power_mw: int = self.bd.machines[machine]["power_mw"]
-
-            item_output: dict = item_book["out"]
-            item_input: dict = item_book["in"]
-
-            # TODO: delete this block for debugging later
-            print("\nITEM:", item)
-            print("OUT: ", item_output)
-            print("IN:  ", item_input)
-
-            # adding machines and power requirements
-            self.__add_int_to_dict(machine, self.machines_needed)
+            machine_name: str = recipe_page["machine"]
+            machine_power_mw: int = self.bd.machines[machine_name]["power_mw"]
+            self.__add_int_to_dict(machine_name, self.machines_needed)
             self.power_mw_needed += machine_power_mw
 
-            # NOTE: now working on items, not machines
-            if item_input == 0:
+            output_dict: dict = recipe_page["out"]
+            input_dict: dict = recipe_page["in"]
+            if input_dict == 0:
                 # early return due to no inputs for current item
                 return
 
+            output_name, output_count = self.__unpack_dict(output_dict)
+            input_name, input_count = self.__unpack_dict(input_dict)
             # check for input being a raw resource
-            if list(item_input.keys())[0] in self.bd.resources:
+            if input_name in self.bd.resources:
                 input_is_raw_resource = True
 
+            # TODO: delete this block for debugging later
+            print("\nITEM:", request_name)
+            print("OUT: ", output_dict)
+            print("IN:  ", input_dict)
+
             # adding item to intermediate materials
-            if (
-                list(item_input.keys())[0] not in self.final_items and
+            input_is_intermediate_material: bool = (
+                input_name not in self.final_items and
                 not input_is_raw_resource
-            ):  # classified as not a raw resource or final item
-                self.__add_value_to_dict(item_input,
-                                         self.inter_materials)
-            elif (
-                input_is_raw_resource
-            ):  # classified as a raw resource, and not a final item
-                # THIS is where we need to do the math
-                self.__add_value_to_dict(item_input, self.raw_materials)
+            )
+            if input_is_intermediate_material:
+                self.__add_value_to_dict(input_dict, self.inter_materials)
+            elif input_is_raw_resource:
+                self.__add_value_to_dict(input_dict, self.raw_materials)
                 pass
 
             # add the inputs to the items_to_visit list
-            if item_input != 0:
-                items_to_visit.append(item_input)
+            if input_dict != 0:
+                items_to_visit.append(input_dict)
