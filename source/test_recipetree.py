b403be9166072b49afe246f62217acfc2803dc83
Nicholas <nicholaszingleman@gmail.com>
2024-12-10 14:57:39 -0500

    updated testing and removed some duplicate items

--- source/test_recipetree.py
+++ source/test_recipetree.py
@@ -14,11 +14,6 @@ class TestRecipeTree(unittest.TestCase):
             iron_plate,
             "Recipe tree name missmatch with root recipe name",
         )
-        self.assertEqual(
-            rt1.root.qty,
-            iron_plate.qty,
-            "Recipe tree qty mismatch with root recipe qty",
-        )
         self.assertEqual(rt1.power_mw, 13, "Incorrect power_mw calculation")
         self.assertEqual(
             [IONode("Iron Ingot", 30)], rt1.inputs, "Intermediate inputs are incorrect"
@@ -31,18 +26,13 @@ class TestRecipeTree(unittest.TestCase):
         # print(rt1)
 
     def test_tree_two(self):
-        reinf_iron_plate: RecipeNode = RecipeNode("Reinforced Iron Plate", 5)
+        reinf_iron_plate: RecipeNode = RecipeNode("Reinforced Iron Plate", 5.0)
         rt2: RecipeTree = RecipeTree(reinf_iron_plate)
 
         self.assertEqual(
             rt2.root,
             reinf_iron_plate,
             "Recipe tree name mismatch with root recipe name",
-        )
-        self.assertEqual(
-            rt2.root.qty,
-            reinf_iron_plate.qty,
-            "Recipe tree qty missmatch with root recipe qty",
         )
         self.assertEqual(rt2.power_mw, 45, "Incorrect power_mw calculation")
         self.assertEqual(
@@ -56,20 +46,25 @@ class TestRecipeTree(unittest.TestCase):
             "Intermediate inputs are incorrect",
         )
         self.assertEqual(
-            rt2.outputs[0].qty,
-            reinf_iron_plate.qty,
+            rt2.outputs[0],
+            IONode("Reinforced Iron Plate", 5.0),
             "Recipe tree output mismatch with input recipe",
         )
         # print(rt2)
 
     def test_tree_three(self):
         # NOTE: testing calcuations of additional qty requested
-        double_screws: RecipeNode = RecipeNode("Screw", 40.0)
+        double_screws: RecipeNode = RecipeNode("Screw", 80.0)
         rt3: RecipeTree = RecipeTree(double_screws)
         self.assertEqual(
             rt3.root,
             double_screws,
             "Recipe tree root recipe mismatch with original recipe",
+        )
+        self.assertEqual(
+            IONode("Screw", 80.0),
+            rt3.outputs[0],
+            "Recipe tree output mismatch with root recipe",
         )
 
         print(rt3)
